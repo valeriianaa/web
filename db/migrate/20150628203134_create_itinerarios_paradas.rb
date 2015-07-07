4f148eaae8453b4038e53abb4b771a9b7274db88
Alejandro Cantero <canteroalejandro@gmail.com>
2015-07-07 00:22:22 -0300

    delete tablas y asociasiones inservibles de itinerarios y paradas

--- db/migrate/20150628203134_create_itinerarios_paradas.rb
+++
@@ -1,8 +0,0 @@
-class CreateItinerariosParadas < ActiveRecord::Migration
-  def change
-    create_table :itinerarios_paradas, id: false do |t|
-      t.belongs_to :itinerario, index: true
-      t.belongs_to :parada, index: true
-    end
-  end
-end
--- db/migrate/20150707001349_add_position_to_itinerarios_paradas.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToItinerariosParadas < ActiveRecord::Migration
-  def change
-    add_column :itinerarios_paradas, :position, :integer
-  end
-end
